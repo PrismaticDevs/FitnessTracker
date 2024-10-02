923b1ae6e08d0dfa13d5e5d22576291f22a4a11f
Matthew Brignola <matthewbrignola@ymail.com>
2024-10-01 20:45:50 -0600

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Matt on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by Matt on 10/1/24.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
