8b839a58eff8e90d4f44d80fc938b52ccbff774e
Matthew Brignola <matthewbrignola@ymail.com>
2025-04-13 21:10:30 -0600

    refactored woekoutentry component into subcomponents to help condense code

--- FitnessTracker/Components/WorkoutEntry.swift
+++ FitnessTracker/Components/WorkoutEntry.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 import SwiftData
 
+var defaults = UserDefaults.standard
+
 struct WorkoutEntryView: View {
-    var defaults = UserDefaults.standard
     @Environment(\.modelContext) var context
     @State var id: UUID = UUID()
     @State var exercise: String
@@ -48,107 +49,20 @@ struct WorkoutEntryView: View {
                         }
                         
                         if defaults.bool(forKey: "iso\(exercise)") {
-                            VStack {
-                                
-                                HStack {
-                                    VStack {
-                                        Text("Left")
-                                            .padding(-4)
-                                            .font(.subheadline)
-                                        TextField("Left Weight", text: $leftInput, prompt: Text("Weight Left").foregroundColor(.white.opacity(0.5)))
-                                            .padding()
-                                            .background(weightInput == "" && leftInput == "" && rightInput == "" ? Color.red.opacity(0.6).cornerRadius(10) : Color.blue.opacity(0.8).cornerRadius(10))
-                                            .onChange(of: leftInput) { oldValue, newValue in
-                                                if let value = Int(newValue) {
-                                                    left = value
-                                                } else {
-                                                    left = 0
-                                                }
-                                                defaults.set(left, forKey: "left\(exercise)")
-                                            }
-                                            .onAppear {
-                                                leftInput = "\(defaults.integer(forKey: "left\(exercise)"))"
-                                            }
-                                    }
-                                    VStack {
-                                        Text("Right")
-                                            .padding(-4)
-                                            .font(.subheadline)
-                                        TextField("Right Weight", text: $rightInput, prompt: Text("Right Weight").foregroundColor(.white.opacity(0.5)))
-                                            .padding()
-                                            .background(weightInput == "" && leftInput == "" && rightInput == "" ? Color.red.opacity(0.6).cornerRadius(10) : Color.blue.opacity(0.8).cornerRadius(10))
-                                            .onChange(of: rightInput) { oldValue, newValue in
-                                                if let value = Int(newValue) {
-                                                    right = value
-                                                } else {
-                                                    right = 0
-                                                }
-                                                defaults.set(right, forKey: "right\(exercise)")
-                                            }
-                                            .onAppear {
-                                                rightInput = "\(defaults.integer(forKey: "right\(exercise)"))"
-                                            }
-                                    }
-                                }
+                            HStack {
+                                WeightField(label: "Left", input: $leftInput, key: "left", exercise: exercise, value: $left)
+                                WeightField(label: "Right", input: $rightInput, key: "right", exercise: exercise, value: $right)
                             }
                         } else {
-                            VStack {
-                                Text("Weight").font(.subheadline)
-                                    .padding(-4)
-                                TextField("Weight", text: $weightInput, prompt: Text("Weight").foregroundColor(.white.opacity(0.5)))
-                                    .padding()
-                                    .background(weightInput == "" && leftInput == "" && rightInput == "" ? Color.red.opacity(0.6).cornerRadius(10) : Color.blue.opacity(0.8).cornerRadius(10))
-                                    .onChange(of: weightInput) { oldValue, newValue in
-                                        if let value = Int(newValue) {
-                                            weight = value
-                                        } else {
-                                            weight = 0
-                                        }
-                                        defaults.set(weight, forKey: "weight\(exercise)")
-                                    }
-                                    .onAppear {
-                                        weightInput = "\(defaults.integer(forKey: "weight\(exercise)"))"
-                                    }
-                            }
+                            WeightField(label: "Weight", input: $weightInput, key: "weight", exercise: exercise, value: $weight)
                         }
                     }
                     
                     HStack {
-                        VStack {
-                            Text("Sets")
-                                .padding(-4)
-                                .font(.subheadline)
-                            TextField("Sets", text: $sets, prompt: Text("Sets").foregroundColor(.white.opacity(0.5)))
-                                .padding()
-                                .background(Color.blue.opacity(0.8).cornerRadius(10))
-                                .onChange(of: sets) { oldValue, newValue in
-                                    sets = newValue
-                                    defaults.set(sets, forKey: "sets\(exercise)")
-                                }
-                        }
-                        VStack {
-                            Text("Reps")
-                                .padding(-4)
-                                .font(.subheadline)
-                            TextField("Reps", text: $reps, prompt: Text("Reps").foregroundColor(.white.opacity(0.5)))
-                                .padding() .background(Color.blue.opacity(0.8).cornerRadius(10))
-                                .onChange(of: reps) { oldValue, newValue in
-                                    reps = newValue
-                                    defaults.set(reps, forKey: "reps\(exercise)")
-                                }
-
-                        }
-                        VStack {
-                            Text("Rest")
-                                .padding(-4)
-                                .font(.subheadline)
-                            TextField("Rest (sec)", text: $rest, prompt: Text("Rest (sec)").foregroundColor(.white.opacity(0.5)))
-                                .padding()
-                                .background(Color.blue.opacity(0.8).cornerRadius(10))
-                                .onChange(of: rest) { oldValue, newValue in
-                                    rest = newValue
-                                    defaults.set(rest, forKey: "rest\(exercise)")
-                                }
+                        HStack {
+                            ExerciseField(label: "Sets", value: $sets, key: "sets", exercise: exercise, placeholder: "Sets")
+                            ExerciseField(label: "Reps", value: $reps, key: "reps", exercise: exercise, placeholder: "Reps")
+                            ExerciseField(label: "Rest", value: $rest, key: "rest", exercise: exercise, placeholder: "Rest")
                         }
                     }
                     Section {
@@ -163,10 +77,10 @@ struct WorkoutEntryView: View {
                                     .lineLimit(1...4)
                                     .overlay(
                                         Button(action: {
-                                            note = "" // Clear the text field
+                                            note = ""
                                         }) {
                                             Image(systemName: "xmark.circle.fill")
-                                                .opacity(note.isEmpty ? 0 : 1) // Hide button if text is empty
+                                                .opacity(note.isEmpty ? 0 : 1)
                                                 .padding()
                                         }
                                         .foregroundColor(Color.white)
@@ -196,7 +110,7 @@ struct WorkoutEntryView: View {
                         }
                     }
                 }
-                .listRowInsets(EdgeInsets()) // Remove default insets
+                .listRowInsets(EdgeInsets())
             WorkoutHistoryView(date: $date, exercise: $exercise, weight: $weight, left: $left, right: $right, sets: $sets, reps: $reps, rest: $rest, note: $note)
         }
         .background(.clear)
@@ -206,6 +120,60 @@ struct WorkoutEntryView: View {
 }
 
 #Preview {
-    WorkoutEntryView(exercise: "", weight: 0, left: 0, right: 0, sets: "", reps: "", rest: "", note: "", onDelete: {})
+    WorkoutEntryView(exercise: "Test Exercise", weight: 0, left: 0, right: 0, sets: "", reps: "", rest: "", note: "", onDelete: {})
+}
+
+// Subcomponents
+struct ExerciseField: View {
+    var label: String
+    @Binding var value: String
+    var key: String
+    var exercise: String
+    var placeholder: String
+
+    var body: some View {
+        VStack {
+            Text(label)
+                .padding(-4)
+                .font(.subheadline)
+            TextField(placeholder, text: $value, prompt: Text(placeholder).foregroundColor(.white.opacity(0.5)))
+                .padding()
+                .background(Color.blue.opacity(0.8).cornerRadius(10))
+                .onChange(of: value) { oldValue, newValue in
+                    value = newValue
+                    defaults.set(value, forKey: "\(key)\(exercise)")
+                }
+        }
+    }
+}
+
+struct WeightField: View {
+    var label: String
+    @Binding var input: String
+    var key: String
+    var exercise: String
+    @Binding var value: Int
+
+    var body: some View {
+        VStack {
+            Text(label)
+                .padding(-4)
+                .font(.subheadline)
+            TextField(label, text: $input, prompt: Text(label).foregroundColor(.white.opacity(0.5)))
+                .padding()
+                .background(input == "" ? Color.red.opacity(0.6).cornerRadius(10) : Color.blue.opacity(0.8).cornerRadius(10))
+                .onChange(of: input) { oldValue, newValue in
+                    if let newValue = Int(newValue) {
+                        value = newValue
+                    } else {
+                        value = 0
+                    }
+                    defaults.set(value, forKey: "\(key)\(exercise)")
+                }
+                .onAppear {
+                    input = "\(defaults.integer(forKey: "\(key)\(exercise)"))"
+                }
+        }
+    }
 }
 
